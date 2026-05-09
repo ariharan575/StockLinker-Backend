3f6eba72a0af9b6cb118818f8e8dd47936af5130
ARIHARAN <fmoon6795@gmail.com>
2026-05-09 20:03:57 +0530

    first commit

---
+++ src/test/java/com/backend/StockLinker/MarketLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.StockLinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketLinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
